6b6d6f355848d51ee7895b5a0a9b05cab514baff
gleb165 <83755952+gleb165@users.noreply.github.com>
2024-08-29 12:28:43 +0300

    implemented a SerializerMethodField

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'courses'
 
 urlpatterns = [
-    path('subject/', views.SubjectListView.as_view(), name='subject_list'),
-    path('subject/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
+    path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
+    path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
 ]
